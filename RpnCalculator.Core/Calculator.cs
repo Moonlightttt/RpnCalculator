8f7e92c099bd44f8448a2bdfc80c9f09a75e7de0
moonlightttt <wwswly@vip.qq.com>
2022-11-01 23:01:23 +0800

    提交

---
+++ RpnCalculator.Core/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace RpnCalculator.Core;
+
+public class Calculator
+{
+    private Stack<decimal> _stack = new Stack<decimal>();
+
+    private List<string> _sourceList;
+
+    public Calculator(string source)
+    {
+        _sourceList = source.Split(" ").ToList().Where(x => x != " ").ToList();
+    }
+
+    public string Execute()
+    {
+        return string.Empty;
+    }
+}
--- RpnCalculator/Program.cs
+++ RpnCalculator/Program.cs
@@ -1,4 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+do
+{
+    Console.Write($"please enter operands:");
+    
+    var str = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(str))
+    {
+        continue;
+    }
+
+    Console.WriteLine($"buffer: {str}");
+    
+} while (true);
